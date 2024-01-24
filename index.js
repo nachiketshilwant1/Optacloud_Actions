3d74a031f704668b1b6f80eee4e3dfdf25d74e2c
nachiket <shilwantn123@gmail.com>
2024-01-24 18:31:32 +0530

    Changes for json parser

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-
 const core = require('@actions/core');
 const axios = require('axios');
 
 // Get the input variables from the workflow file
-const city = core.getInput('city');
 const api_key = core.getInput('api_key');
 
 // Define the API endpoint and parameters
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+const url = "https://pluginactions.onrender.com/analyze/data";
 const params = {
-  q: city,
-  appid: api_key,
-  units: 'metric'
+  token: api_key,
 };
 
 // Make a GET request to the API and handle the response
 axios.get(url, {params: params})
   .then(response => {
-    const temp = response.data.main.temp;
-    console.log(`The temperature in ${city} is ${temp} °C`);
-    if (temp > 30) {
-      core.setFailed(`The temperature in ${city} is too high for deployment`);
+    const msg = response.data.message;
+    console.log(JSON.stringify(response.data.result));
+    if (msg == "fail") {
+      core.setFailed(`Token is not present`);
       core.setOutput('status', 'failed');
     } else {
+      // core.setOutput('api_response', JSON.stringify(response.data));
       core.setOutput('status', 'success');
     }
   })
